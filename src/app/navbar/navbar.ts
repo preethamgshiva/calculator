89dbed7a8262f3d8fa362eb2f7d6c691292e39e1
preethamgshiva <preethamgshiva2004@gmail.com>
2025-09-29 11:15:30 +0530

    Add Back Button Changes

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,25 +1,45 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIf],
   template: `
     <div class="bg-primary-subtle w-100 p-3">
       <nav class="d-flex align-items-center pt-3 pb-1 position-relative">
 
-        <button class="btn btn-secondary position-absolute start-0 ms-3" (click)="goBack()">Back</button>
+        <!-- Back button only shows when NOT on / or /hyperlinks -->
+        <button 
+          *ngIf="!hideBackButton" 
+          class="btn btn-secondary position-absolute start-0 ms-3" 
+          (click)="goBack()">Back</button>
 
         <h3 class="mx-auto mb-0">Calculator</h3>
 
+        <!-- Home button -->
         <button class="btn btn-primary position-absolute end-0 me-3" (click)="goHome()">Home</button>
       </nav>
     </div>
   `,
-  styleUrl: './navbar.css'
+  styleUrls: ['./navbar.css']
 })
 export class Navbar {
+  hideBackButton = false;
+
+  constructor(private router: Router) {
+    // Listen for navigation changes
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const currentUrl = event.urlAfterRedirects;
+        this.hideBackButton = currentUrl === '/' || currentUrl === '/hyperlinks';
+      });
+  }
+
   goHome() {
-    window.location.href = '/';
+    this.router.navigate(['/']);
   }
 
   goBack() {
